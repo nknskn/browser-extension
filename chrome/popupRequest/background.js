62ec04d18845f7273bd72f61083dea8054b8a508
nknskn <kroorz.gnkknv@gmail.com>
2019-02-12 23:36:28 +0900

    Chrome/popupRequest - first commit

---
+++ chrome/popupRequest/background.js
@@ -0,0 +1,25 @@
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    if(details.requestBody) {
+      if (!('error' in details.requestBody)){
+        if ('raw' in details.requestBody) {
+          postedString = '';
+          for (var i = 0; i < details.requestBody.raw.length; i++) {
+            postedString += decodeURIComponent(String.fromCharCode.apply(null, new Uint8Array(details.requestBody.raw[i].bytes)));
+          }          
+        } else if ('formData' in details.requestBody) {
+          postedString = '{';
+          for (var k in details.requestBody.formData) {
+            postedString += '"' + k + '":"' + details.requestBody.formData[k] + '",';
+          }
+          postedString += '}';
+        }
+        alert(details.url + "\r\n" + postedString);
+      } else {
+        alert(details.url)
+      }
+    }
+  },
+  {urls: ["<all_urls>"]},
+  ["requestBody"]
+);
